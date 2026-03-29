48b274456ae2429a0c83bd0cd2404525035c51a4
nhanchidanh1 <150265023+nhanchidanh1@users.noreply.github.com>
2026-03-29 23:30:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nhanchidanh1/',
+  base: 'https://nhanchidanh1.github.io/chicken-invaders/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
